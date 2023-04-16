c4eedb0cbb6a3cb12d0976d5d9ec70145da1e44f
LunexCoding <lunexcoding@vk.com>
2023-04-17 04:56:19 +0700

    Add core logic

--- main.py
+++ main.py
@@ -1,74 +1,11 @@
 import sys
-import random
-import json
 
-import uvicorn
-from fastapi import Depends, FastAPI
-from fastapi.responses import JSONResponse
-
-from helpers.database import databaseSession
-from helpers.sqlQueries import SqlQueries
-from settingsConfig import PrettyJSONResponse
-
-
-def __initTables():
-    with databaseSession as db:
-        commands = [
-            SqlQueries.createTableWriter(),
-            SqlQueries.createTableBook()
-        ]
-        for command in commands:
-            db.execute(command)
-
-def __initData():
-    with databaseSession as db:
-        for writerID in range(1, 4):
-            name = f"writer_{writerID}"
-            db.execute(
-                SqlQueries.insertWriter(name),
-                dict(
-                    name=name
-                )
-            )
-            numberAuthorBooks = random.randint(1, 4)
-            print(numberAuthorBooks)
-            for authorBookID in range(1, numberAuthorBooks + 1):
-                bookName = f"book {authorBookID} from author {writerID}"
-                print(authorBookID, bookName)
-                db.execute(
-                    SqlQueries.insertBook(writerID, bookName),
-                    dict(
-                        author_id=writerID,
-                        name=bookName
-                    )
-                )
-
-def initDatabase():
-    print("init")
-    __initTables(),
-    __initData()
-
-
-app = FastAPI()
-
-@app.get("/writers/{id}", response_class=PrettyJSONResponse)
-def get_account(id):
-    writerData = None
-    with databaseSession as db:
-        writerData = db.getRows(
-            SqlQueries.selectFullDataAboutWriter(id),
-            data=dict(
-                writerID=id
-            )
-        )
-    if writerData is None:
-        return JSONResponse(status_code=404, content={"ErrorMessage": "Writer not found"})
-    return writerData[0]
+from server import server, app
 
 
 if __name__ == "__main__":
     args = sys.argv
-    if "init" in args:
-        initDatabase()
-    uvicorn.run(app, host="127.0.0.1", port=8000)
-
+    app.include_router(server.router)
+    server.setInitOnStart("init" in args)
+    server.setDropOnFinish("drop" in args)
+    server.start()
